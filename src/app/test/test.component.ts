699603628d21376e55e78ea0fc57e9f8458eced4
PujanZZ <71079402+PujanZZ@users.noreply.github.com>
2023-02-02 11:29:53 +0530

    1 file modified

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -160,15 +160,18 @@ export class TestComponent implements OnInit {
   }
 }
 
-function emailDomain(control: AbstractControl): { [key: string]: any } | null {
-  const email: string = control.value;
-  const domain = email.substring(email.lastIndexOf('@') + 1);
+//closures - function inside another function(inner fn has access to outer fn parameters and var in addition to its own)
+function emailDomain(domainName: string) {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const email: string = control.value;
+    const domain = email.substring(email.lastIndexOf('@') + 1);
 
-  if (email === '' || domain.toLowerCase() === 'gmail.com') {
-    return null;
-  } else {
-    return {
-      emailDomain: true,
-    };
-  }
+    if (email === '' || domain.toLowerCase() === 'gmail.com') {
+      return null;
+    } else {
+      return {
+        emailDomain: true,
+      };
+    }
+  };
 }
