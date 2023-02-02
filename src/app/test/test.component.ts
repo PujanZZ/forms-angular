c08d96238f03159d8966f6be640ff7d46c431f16
PujanZZ <71079402+PujanZZ@users.noreply.github.com>
2023-02-02 12:41:01 +0530

    1 file modified

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -172,19 +172,3 @@ export class TestComponent implements OnInit {
     phoneControl.updateValueAndValidity();
   }
 }
-
-//closures - function inside another function(inner fn has access to outer fn parameters and var in addition to its own)
-// function emailDomain(domainName: string) {
-//   return (control: AbstractControl): { [key: string]: any } | null => {
-//     const email: string = control.value;
-//     const domain = email.substring(email.lastIndexOf('@') + 1);
-
-//     if (email === '' || domain.toLowerCase() === domainName.toLowerCase()) {
-//       return null;
-//     } else {
-//       return {
-//         emailDomain: true,
-//       };
-//     }
-//   };
-// }
